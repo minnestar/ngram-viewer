daf922aad7ecc6895f74de4052c2c3cc7115f423
Dan F <dfrankow@gmail.com>
2012-04-22 08:25:01 -0500

    protect against /s in pagenames

--- download_data.rb
+++ download_data.rb
@@ -17,12 +17,21 @@ def download_page_names
   # returned JSON is invalid, so we have to hand-edit it :(
 end
 
+def safe_filename(name)
+  name = name.gsub(/\//, '_')
+  "raw/#{name}"
+end
+
 def download_page_text(title)
-  uri = URI("http://#{WIKI_SITE}/w/index.php")
-  params = { :title => title, :action => 'raw' }
-  uri.query = URI.encode_www_form(params)
-  result = Net::HTTP.get(uri)
-  IO.write("raw/#{title}", result)
+  filename = safe_filename(title)
+  if !File.exists?(filename)
+    warn "Filename #{filename}"
+    uri = URI("http://#{WIKI_SITE}/w/index.php")
+    params = { :title => title, :action => 'raw' }
+    uri.query = URI.encode_www_form(params)
+    result = Net::HTTP.get(uri)
+    IO.write(filename, result)
+  end
 end
 
 def main
